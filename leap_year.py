ee30a3612008014236560d9c891bc01aabf75cf9
Beltranmarco <bmarco@mail.austral.edu.ar>
2025-06-12 19:11:15 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,7 @@
 def leap_year():
-    print("TO DO")
+    year = int(input(""))
+
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print(f"el año {year} no es biciesto")
+    else: 
+        print(f"el año {year} no es biciesto")
