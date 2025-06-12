931edccf8986f1c8134b10fcca0890f1b31999ed
Beltranmarco <bmarco@mail.austral.edu.ar>
2025-06-12 19:39:26 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,27 +1,31 @@
 def line():
-    A = float(input("Ingrese el coeficiente A: "))
-    B = float(input("Ingrese el coeficiente B: "))
-    X1 = float(input("Ingrese el coeficiente X1: "))
-    X2 = float(input("Ingrese el coeficiente X2: "))
+        A = input("Ingrese el coeficiente A: ")
+        B = input("Ingrese el coeficiente B: ")
+        X1 = input("Ingrese el coeficiente X1: ")
+        X2 = input("Ingrese el coeficiente X2: ")
 
-    print("El coeficiente A de su ecuación de la recta es:", A)
-    print("El coeficiente B de su ecuación de la recta es:", B)
-    print("El coeficiente X1 de su ecuación de la recta es:", X1)
-    print("El coeficiente X2 de su ecuación de la recta es:", X2)
+        print("El coeficiente A de su ecuación de la recta es: " + format(float(A), '.1f'))
+        print("El coeficiente B de su ecuación de la recta es: " + format(float(B), '.1f'))
+        print("El coeficiente X1 de su ecuación de la recta es: " + format(float(X1), '.1f'))
+        print("El coeficiente X2 de su ecuación de la recta es: " + format(float(X2), '.1f'))
 
-    print()
-    print("Para la siguiente ecuación:")
-    print("Y = ({})x + ({})".format(A, B))
+        A = float(A)
+        B = float(B)
+        X1 = float(X1)
+        X2 = float(X2)
 
-    Y1 = A * X1 + B
-    Y2 = A * X2 + B
+        print()
+        print("Para la siguiente ecuación:")
+        print("Y = ({})x + ({})".format(A, B))
 
-    print()
-    print("Dados los siguientes puntos:")
-    print("\tP1 ({}, {})".format(X1, Y1))
-    print("\tP2 ({}, {})".format(X2, Y2))
+        Y1 = A * X1 + B
+        Y2 = A * X2 + B
 
-    D = ((X2 - X1)**2 + (Y2 - Y1)**2)**0.5
+        print()
+        print("Dados los siguientes puntos:")
+        print("\tP1 ({}, {})".format(X1, Y1))
+        print("\tP2 ({}, {})".format(X2, Y2))
 
-    print()
-    print("La distancia entre ellos es:", D)
+        D = ((X2 - X1)**2 + (Y2 - Y1)**2)**0.5
+        print()
+        print("La distancia entre ellos es: {}".format(D))
