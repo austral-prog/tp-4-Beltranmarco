6243976f0cb8569224d0c8f0a0af4858f306d5ca
Beltranmarco <bmarco@mail.austral.edu.ar>
2025-06-12 19:15:59 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,7 +1,32 @@
-def leap_year():
-    year = int(input(""))
+def line():
+    A = input("Ingrese el coeficiente A: ")
+    B = input("Ingrese el coeficiente B: ")
+    X1 = input("Ingrese el coeficiente X1: ")
+    X2 = input("Ingrese el coeficiente X2: ")
 
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        print(f"El año {year} es bisiesto")
-    else:
-        print(f"El año {year} no es bisiesto")
+    print("El coeficiente A de su ecuación de la recta es: " + format(float(A), '.1f'))
+    print("El coeficiente B de su ecuación de la recta es: " + format(float(B), '.1f'))
+    print("El coeficiente X1 de su ecuación de la recta es: " + format(float(X1), '.1f'))
+    print("El coeficiente X2 de su ecuación de la recta es: " + format(float(X2), '.1f'))
+
+    A = float(A)
+    B = float(B)
+    X1 = float(X1)
+    X2 = float(X2)
+
+    print()
+    print("Para la siguiente ecuación:")
+    print("Y = ({})x + ({})".format(A, B))
+
+    Y1 = A * X1 + B
+    Y2 = A * X2 + B
+
+    print()
+    print("Dados los siguientes puntos:")
+    print("\tP1 ({}, {})".format(X1, Y1))
+    print("\tP2 ({}, {})".format(X2, Y2))
+
+    D = ((X2 - X1)**2 + (Y2 - Y1)**2)**0.5
+
+    print()
+    print("La distancia entre ellos es: {}".format(D))
